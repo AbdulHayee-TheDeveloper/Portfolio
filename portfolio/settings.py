9673b421eda50c1648fcc7810e785b47a7601da6
Abdul Hayee Chaudhary <ahayee535@gmail.com>
2026-07-30 17:02:24 +0500

    Envirment variable updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -157,7 +157,14 @@ STORAGES = {
     },
 }
 
+import cloudinary
 
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+    secure=True,
+)
 
 DEFAULT_FROM_EMAIL = 'your_email@example.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
